9cbfa90622eba25680d86a52b33264fb61e5fd5a
asjadanis <asjadanis128@gmail.com>
2022-09-19 17:19:05 -0500

    chore: add simple course & student page

---
+++ react-assignment-7/src/pages/Course/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Course: FC = () => {
+  return (
+    <section>
+      <h1> Courses </h1>
+    </section>
+  );
+};
+
+export default Course;
---
+++ react-assignment-7/src/pages/Student/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Student: FC = () => {
+  return (
+    <section>
+      <h1>Student</h1>
+    </section>
+  );
+};
+
+export default Student;
