b044b9d672b0e68a4ca8a5fd81569c864c661cff
tewelle <tewelle.welemariam@cag.se>
2022-09-20 22:29:26 -0500

    Tewelle: Added React components, axios calls, useEffects, useState and props.

---
+++ src/routes/Router.ts
@@ -0,0 +1,8 @@
+import CategoryComponent from "../components/CategoriesComponent"
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    Link,
+} from "react-router-dom";
+
---
+++ src/routes/categoryRoutes.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import Products from "../components/ProductsComponent";
+import Categories from "../components/CategoriesComponent";
+
+export default function CategoryRoutes() {
+  return (
+      <Categories></Categories>
+  );
+}
